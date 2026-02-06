a6a477d2a0c570d06cac2496f30264d471b17cb6
TANJAWI <ahmedahmed2007tanjawi@gmail.com>
2026-02-06 17:23:55 +0100

    Almost to be finished, just need displaying winner and some improvemnents

--- js/HangManJS.js
+++ js/HangManJS.js
@@ -14,6 +14,8 @@ const CATEGORIES = {
 const div_letters = document.getElementById("letters");
 const categories = document.getElementById("categories");
 const message = document.getElementById("message");
+const div_images = document.getElementById('hangman-images')
+const displayed_image = document.getElementById('displayed-image')
 
 const complete_missing_letters = document.getElementById("complete-word");
 const choiceButton = document.getElementById("confirmChoice");
@@ -22,6 +24,8 @@ const repeatButton = document.getElementById("repeatButton");
 
 let selectedCategory = [];
 let target = [];
+let trackImagesCount = 0;
+let isCompleted = 0; // check the number of the found letter of the missing word.
 
 // Confirming the choice of category
 function confirmChoice() {
@@ -30,7 +34,7 @@ function confirmChoice() {
     if (CATEGORIES[userChoice]) {
       selectedCategory = CATEGORIES[userChoice];
       target = [...getRandomWord()];
-      message.textContent = `You've chosen your category: ${userChoice.toUpperCase()}`;
+      message.textContent = `You've chosen your category: ${userChoice.toUpperCase()}. 5 chances`;
       message.style.color = "#0b07f0";
       choiceButton.style.display = "none";
       submitLetterButton.style.display = "inline";
@@ -63,7 +67,7 @@ function showFirstHint() {
             element.style.color = 'transparent';
         }
         else {
-            element.style.color = 'red';
+            element.style.color = '#ff0000';
         }
     });
 }
@@ -72,27 +76,61 @@ function showFirstHint() {
 function play() {
     let letter = complete_missing_letters.value.toUpperCase();
     const lettersQueury = document.querySelectorAll("#letters p");
+    let isCorrect = false
 
     lettersQueury.forEach(element => {
         if (element.textContent === letter) {
             element.style.color = '#ff0000'
-        }
-        else {
-            // Complete images if the gusses are wrong
+            isCorrect = true
         }
     })
+
+    if (!isCorrect) {
+        message.textContent = `The letter '${letter.toLowerCase()}' is wrong!!`
+        message.style.color = '#f8720b'
+        displayImage(trackImagesCount)
+        trackImagesCount++;
+    }
+    else {
+        checkWin(lettersQueury)
+    }
 }
 
-// Repeat the game
+// Display Hang man image if the user types a wrong letter. (part of play function)
+function displayImage (trackImagesCount) {
+    const images = ['head.png', 'left-arm.png', 'right-arm.png', 'left-leg.png', 'full-Hang-Man-Body.png']
+    displayed_image.src = `images/HangManImages/${images[trackImagesCount]}`
+
+    if ((images.length - 1) === trackImagesCount) {
+        message.textContent = `You lost the game, the image now is completed. Press repeat to play again`
+        message.style.color = '#ff0000'
+        submitLetterButton.style.disabled = true
+    }
+}
+
+// Check if the user complete all the letter before completing the Hang Man image. (part of play function)
+function checkWin (lettersQueury) {
+    lettersQueury.forEach((element) => { if (element.style.color == '#ff0000') isCompleted++; })
+    
+    if (isCompleted == (target.length - 1)) {
+        message.textContent = `You won the game. You completed the missing word.!!`
+        message.style.color = '#00ff08'
+        submitLetterButton.style.disabled = false
+    }
+}
+
 function repeat() {
     selectedCategory = []
     target = []
+    trackImagesCount = 0
+    isCompleted = 0
     message.textContent = '';
     complete_missing_letters.value = '';
     complete_missing_letters.style.display = 'none'
     submitLetterButton.style.display = 'none'
     repeatButton.style.display = 'none'
     choiceButton.style.display = 'inline'
+    displayed_image.src = 'images/HangManImages/Empty-gallows.png'
     
     // Remove displayed letters.
     let lettersQueury = document.querySelectorAll("#letters p");
@@ -100,13 +138,11 @@ function repeat() {
     lettersQueury = '';
 }
 
-// Get a random word from the select category.
 function getRandomWord() {
     const index = Math.floor(Math.random() * selectedCategory.length);
     return selectedCategory[index];
 }
 
-// Get the first hint (letter).
 function getRandomLetter(target) {
     return Math.floor(Math.random() * target.length);
 }
